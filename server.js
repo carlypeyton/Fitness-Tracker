39d7f11fcc03b82b397006e76c30a9d23b749704
Carly Peyton <carlypeyton@gmail.com>
2021-04-04 11:54:32 -0400

    Create server file.

---
+++ server.js
@@ -0,0 +1,37 @@
+//NPM dependencies
+const express = require("express");
+const logger = require("morgan");
+const mongoose = require("mongoose");
+
+//Set Port
+const PORT = process.env.PORT || 3000;
+
+//Set Express
+const app = express();
+
+//Data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+//Public folder is static
+app.use(express.static("public"));
+
+//Use Logger
+app.use(logger("dev"));
+
+//Mongoose connect
+mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
+    {
+      useNewUrlParser: true,
+      useFindAndModify: false
+    }
+  );
+
+//Require Routes
+app.use(require("./routes/api.js"));
+app.use(require("./routes/html.js"));
+
+//Listening on PORT
+app.listen(PORT, () => {
+    console.log(`App running on port ${PORT}!`);
+});
