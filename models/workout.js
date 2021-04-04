a3d0178185ad6586611609bf7037368b19c07b45
Carly Peyton <carlypeyton@gmail.com>
2021-04-04 11:56:39 -0400

    Update package files. Formatting. Create index and workout models.

---
+++ models/workout.js
@@ -0,0 +1,54 @@
+const mongoose = require("mongoose");
+
+const Schema = mongoose.Schema;
+
+const WorkoutSchema = new Schema({
+  day: {
+    type: Date,
+    default: Date.now
+  },
+  exercises: [
+    {
+      type: {
+        type: String,
+        trim: true,
+        required: "Please enter workout type."
+
+      },
+      name: {
+        type: String,
+        trim: true,
+        required: "Please enter workout name."
+      },
+      distance: {
+        type: Number,
+        default: 0
+      },
+      duration: {
+        type: Number,
+        default: 0,
+        required: "Please enter workout duration (minutes)."
+      },
+      weight: {
+        type: Number,
+        default: 0,
+      },
+      sets: {
+        type: Number,
+        default: 0
+      },
+      reps: {
+        type: Number,
+        default: 0
+      },
+    }
+  ],
+  totalDuration: {
+    type: Number,
+    default: 0,
+  }
+});
+
+const Workout = mongoose.model("Workout", WorkoutSchema);
+
+module.exports = Workout;
