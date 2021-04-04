6814ab89e6c2679f54d1a46e4eb2313dd192d528
Carly Peyton <carlypeyton@gmail.com>
2021-04-04 14:16:45 -0400

    Deploy to heroku and connect to MongoDB Atlas.

--- server.js
+++ server.js
@@ -20,12 +20,15 @@ app.use(express.static("public"));
 app.use(logger("dev"));
 
 //Mongoose connect
-mongoose.connect(process.env.MONGODB_URI || "mongodb://localhost/workout", 
-    {
-      useNewUrlParser: true,
-      useFindAndModify: false
-    }
-  );
+mongoose.connect(
+  process.env.MONGODB_URI || 'mongodb://localhost/Workout',
+  {
+    useNewUrlParser: true,
+    useUnifiedTopology: true,
+    useCreateIndex: true,
+    useFindAndModify: false
+  }
+);
 
 //Require Routes
 app.use(require("./routes/api.js"));
