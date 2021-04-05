8552851e2fc88ea8bfefb453714b9484d01e16d5
Carly Peyton <carlypeyton@gmail.com>
2021-04-04 20:36:15 -0400

    Update formatting. Add html route for index. Total Duration still not functional.

--- routes/html.js
+++ routes/html.js
@@ -2,6 +2,11 @@
 const router = require("express").Router();
 const path = require("path");
 
+//Index route - default route
+router.get("/", (req, res) => {
+  res.sendFile(path.join(__dirname + "/index.html"));
+});
+
 //Get exercise HTML
 router.get("/exercise", (req, res) => {
   res.sendFile(path.join(__dirname, "../public/exercise.html"));
