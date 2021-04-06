df900ecc8481cf44769c0cbf49eea9f38617e5b0
Carly Peyton <carlypeyton@gmail.com>
2021-04-06 11:58:15 -0400

    Updates.

--- server.js
+++ server.js
@@ -2,6 +2,7 @@
 const express = require("express");
 const logger = require("morgan");
 const mongoose = require("mongoose");
+// require("./seeders/seed");
 
 //Set Port
 const PORT = process.env.PORT || 3000;
